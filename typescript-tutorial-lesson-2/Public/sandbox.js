5bc9374d4c7d131e508551db01d6f7548705e333
yuosif1286 <84059356+yuosif1286@users.noreply.github.com>
2023-12-04 16:06:08 -0800

    completed function parameter type

--- typescript-tutorial-lesson-2/Public/sandbox.js
+++ typescript-tutorial-lesson-2/Public/sandbox.js
@@ -1,3 +1,18 @@
 "use strict";
-console.log("Test");
-console.log(63);
+// let greet= () => {
+//     console.log("hello , world");
+// }
+let greet;
+// greet='hello';
+greet = () => {
+    console.log("Hello , world");
+};
+greet();
+const add = (a, b, c = 10) => {
+    console.log(a + b);
+    console.log(c);
+};
+add(5, 3, "yoyo");
+const minus = (a, b) => a - b;
+let result = minus(45, 5);
+console.log(result);
--- typescript-tutorial-lesson-2/src/sandbox.ts
+++ typescript-tutorial-lesson-2/src/sandbox.ts
@@ -1,2 +1,25 @@
-console.log("Test");
-console.log(63);
+// let greet= () => {
+//     console.log("hello , world");
+    
+// }
+
+let greet :Function;
+// greet='hello';
+
+greet =() => {console.log("Hello , world");
+}
+
+greet();
+
+const add = (a:number,b: number,c:number|string =  10)=>{
+    console.log(a+b);
+    console.log(c);
+}
+
+add(5,3,"yoyo");
+
+const minus =(a: number ,b : number) => a-b;
+
+let result= minus(45,5);
+
+console.log(result);
