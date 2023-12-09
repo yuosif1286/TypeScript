cc405d3de3033bc90947421ea63fd3f3c3afc69d
yuosif <yuosifraed@gmail.com>
2023-12-09 13:31:29 +0300

    Completed

--- typescript-tutorial-lesson-2/Public/app.js
+++ typescript-tutorial-lesson-2/Public/app.js
@@ -1,4 +1,20 @@
 "use strict";
+//Classes
+class Invoice {
+    constructor(c, d, a) {
+        this.client = c;
+        this.details = d;
+        this.amount = a;
+    }
+    format() {
+        console.log(`${this.client} owes # ${this.amount} for ${this.details}`);
+    }
+}
+const invOne = new Invoice('yoyo', 'work in kf', 493);
+const invTwo = new Invoice('mohamed', 'work in kf', 898);
+let invoices = [];
+invoices.push(invOne, invTwo);
+console.log(invoices);
 // const anchor=document.querySelector('a') as HTMLAnchorElement;
 // anchor.innerText="yoyo";
 // console.log(anchor.href);
--- typescript-tutorial-lesson-2/src/app.ts
+++ typescript-tutorial-lesson-2/src/app.ts
@@ -1,3 +1,34 @@
+//Classes
+
+class Invoice{
+client: string;
+details:string;
+amount:number;
+
+
+constructor (c:string,d:string,a:number)
+{
+    this.client=c;
+    this.details=d;
+    this.amount=a;
+}
+
+format(){
+    console.log(`${this.client} owes # ${this.amount} for ${this.details}`);
+}
+}
+
+const invOne= new Invoice('yoyo','work in kf',493);
+const invTwo= new Invoice('mohamed','work in kf',898);
+
+
+let invoices : Invoice[]=[];
+
+invoices.push(invOne,invTwo); 
+
+console.log(invoices);
+
+
 // const anchor=document.querySelector('a') as HTMLAnchorElement;
 // anchor.innerText="yoyo";
 // console.log(anchor.href);
